1389832be83cb872ee8e8bd1e9d3f2275199e157
pWillOliveira <wpo_0072@hotmail.com>
2022-11-25 21:24:50 -0300

    Adicionado classes Mentoria, Curso e Main

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição Curso Java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Javascript");
+		curso2.setDescricao("Descrição Curso Javascript");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria de Java");
+		mentoria.setDescricao("Descrição Mentoria Java");
+		mentoria.setData(LocalDate.now());
+
+		
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+		
+
+	}
+
+}
